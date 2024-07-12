690036206d1ee443b5a843c2874324e064706ead
Haorsis <cadetgauthier3@gmail.com>
2024-07-12 15:05:36 +0200

    implement data model

---
+++ src/MNS.Iot.Backend.Domain/Magasins/Passerelle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+
+namespace MNS.Iot.Backend.Magasins {
+    public class Passerelle : Entity<Guid> {
+        public List<Sonde> Sondes { get; set; }
+
+        public Passerelle(List<Sonde> sondes) {
+            Sondes = sondes;
+        }
+
+        public void AjouterMesure(Guid sondeId, double temperature) {
+            Sonde sonde = Sondes.First((s) => s.Id == sondeId);
+            sonde.AjouterMesure(temperature);
+        }
+        public IEnumerable<Sonde> RecupererSondes() {
+            return Sondes;
+        }
+
+        public IEnumerable<Mesure> RecupererMesures(Guid sondeId) {
+            Sonde sonde = Sondes.First((s) => s.Id == sondeId);
+            return sonde.RecupererMesures();
+        }
+
+    }
+}
---
+++ src/MNS.Iot.Backend.Domain/Magasins/Sonde.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Volo.Abp.Domain.Entities;
+
+namespace MNS.Iot.Backend.Magasins {
+    public class Sonde : Entity<Guid> {
+        public List<Mesure> Mesures { get; set; }
+
+        public Sonde(List<Mesure> mesures) {
+            Mesures = mesures;
+        }
+
+        public void AjouterMesure(double temperature) {
+            Mesures.Add(new Mesure(temperature));
+        }
+
+        internal IEnumerable<Mesure> RecupererMesures() {
+            return Mesures;
+        }
+    }
+}
