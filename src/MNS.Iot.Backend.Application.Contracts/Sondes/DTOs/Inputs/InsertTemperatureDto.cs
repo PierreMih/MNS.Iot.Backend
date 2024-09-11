fd47f1c74bbd7e44c30ade2096e3046eb218d953
Haorsis <cadetgauthier3@gmail.com>
2024-09-11 13:32:26 +0200

    - add : dto

---
+++ src/MNS.Iot.Backend.Application.Contracts/Sondes/DTOs/Inputs/InsertTemperatureDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MNS.Iot.Backend.Sondes.DTOs.Inputs {
+    public class InsertTemperatureDto {
+        private string Sonde_id {  get; set; }
+        private string Temperature { get; set; }
+        
+        private string Timestamp { get; set; }
+    }
+}
